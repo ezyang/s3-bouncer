4683330f1fd6999f5416980830adb9bdd38e021f
Edward Z. Yang <ezyang@cs.stanford.edu>
2018-09-04 11:21:22 -0400

    Handle Heroku proxy
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, request
 import boto3
 import uuid
 import dns.resolver
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=1)
 
 @app.route('/')
 def main():
